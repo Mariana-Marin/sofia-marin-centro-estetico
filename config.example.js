31f52c6df12697f2c9801b8934761552a3133083
Mariana Marin <marianamarinvanegas2014@gmail.com>
2026-03-22 00:13:59 -0500

    Extraer apikeys a archivo config.js omitido en git

---
+++ config.example.js
@@ -0,0 +1,11 @@
+﻿const ENV = {
+  ANTHROPIC_API_KEY: "TU_API_KEY_AQUI",
+  FIREBASE: {
+    apiKey: "TU_API_KEY_AQUI",
+    authDomain: "TU_DOMINIO.firebaseapp.com",
+    projectId: "TU_PROJECT_ID",
+    storageBucket: "TU_BUCKET.firebasestorage.app",
+    messagingSenderId: "TU_MESSAGING_SENDER_ID",
+    appId: "TU_APP_ID"
+  }
+};
